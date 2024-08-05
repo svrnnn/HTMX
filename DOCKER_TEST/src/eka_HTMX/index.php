e9cf1a944243df76de9d27ef10e8a9ab0bb53e28
svrnnn <sallavornanen@hotmail.com>
2024-08-05 19:49:56 +0300

    HTMX
    Harjoitukset

---
+++ DOCKER_TEST/src/eka_HTMX/index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eka HTMX</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="main-header">
+        <img src="htmx-logo.png" alt="HTMX logo">
+        <h1>high power tools for HTML</h1>
+    </header>
+    <main>
+        <p>you can build modern user interfaces with the simplicity and power of hypertext</p>
+        <button>Learn more</button>
+    </main>
+    <footer></footer>
+</body>
+</html>
