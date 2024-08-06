502ba48dac4612746f59fa4c8b89f104361d26be
svrnnn <sallavornanen@hotmail.com>
2024-08-06 19:35:16 +0300

    2024-08-06

--- DOCKER_TEST/src/eka_HTMX/index.php
+++ DOCKER_TEST/src/eka_HTMX/index.php
@@ -1,9 +1,15 @@
+<?php 
+    include 'funcs.php';
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eka HTMX</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://unpkg.com/htmx.org@2.0.1" integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/" crossorigin="anonymous"></script>
 </head>
 <body>
     <header id="main-header">
@@ -11,8 +17,23 @@
         <h1>high power tools for HTML</h1>
     </header>
     <main>
-        <p>you can build modern user interfaces with the simplicity and power of hypertext</p>
-        <button>Learn more</button>
+        <p>You can build modern user interfaces with the simplicity and power of hypertext</p>
+        <form hx-post="note.php">
+            <label for="note">Your note here</label>
+            <input type="text" id="note" name="note">
+            <button>Save Button</button>
+        </form>
+        <? // generateList(); ?>
+        
+        <?php include 'info.php';?>
+        <!--
+        <button 
+        hx-get="info.php"
+        hx-trigger="mouseenter[ctrlKey] once, click once"
+        hx-target="main" 
+        hx-swap="beforeend"
+        >Learn more</button>
+        -->
     </main>
     <footer></footer>
 </body>
